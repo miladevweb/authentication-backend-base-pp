a7a99d5bdda0f2f6cbfcdfcec11550a2315a209e
miladevweb <miladevweb@outlook.com>
2024-11-11 17:05:36 -0500

    fix: use environment variables in cloudinary config

--- src/utils/CloudinaryConfig.ts
+++ src/utils/CloudinaryConfig.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary'
 
 cloudinary.config({
-  cloud_name: 'dzfnauf8j',
-  api_key: '241818423677337',
-  api_secret: 'x4zqBAvUY_h3Ljk86doN-tXFOFI',
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLODINARY_API_SECRET!,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
 })
 
 export default cloudinary
