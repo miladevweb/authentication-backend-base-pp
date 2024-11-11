b040476a09f9dc51a330dc57e9a49c7fe81dac12
miladevweb <miladevweb@outlook.com>
2024-11-11 16:05:05 -0500

    fix: delete dist folder from github

--- dist/utils/CloudinaryConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cloudinary_1 = require("cloudinary");
-cloudinary_1.v2.config({
-    cloud_name: 'dzfnauf8j',
-    api_key: '241818423677337',
-    api_secret: 'x4zqBAvUY_h3Ljk86doN-tXFOFI',
-});
-exports.default = cloudinary_1.v2;
